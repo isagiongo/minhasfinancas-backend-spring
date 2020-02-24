a39699cea4a00e9e9aaee3ffaef4b7559a8078ac
Isa Giongo <isagiongo@gmail.com>
2020-02-24 10:51:05 -0300

    Refatora teste unitário do service utilizando mocks

--- src/test/java/com/isagiongo/appfinancas/services/UsuarioServiceTest.java
+++ src/test/java/com/isagiongo/appfinancas/services/UsuarioServiceTest.java
@@ -1,41 +1,46 @@
 package com.isagiongo.appfinancas.services;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.isagiongo.appfinancas.exceptions.RegraNegocioException;
-import com.isagiongo.appfinancas.model.entities.Usuario;
 import com.isagiongo.appfinancas.repositories.UsuarioRepository;
+import com.isagiongo.appfinancas.services.impl.UsuarioServiceImpl;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
 public class UsuarioServiceTest {
 
-	@Autowired
 	private UsuarioService usuarioService;
 	
-	@Autowired
+	@Mock
 	private UsuarioRepository usuarioRepository;
+	
+	@BeforeEach
+	public void setUp() {
+		usuarioService = new UsuarioServiceImpl(usuarioRepository);
+	}
 	
 	@Test
 	public void deveValidarEmail() {
-		usuarioRepository.deleteAll();
+		when(usuarioRepository.existsByEmail("teste@gmail.com")).thenReturn(Boolean.FALSE);
 		
 		assertDoesNotThrow(() -> usuarioService.validarEmail("teste@gmail.com"));
 	}
 	
 	@Test
 	public void deveRetornarExceptionSeEmailExistir() {
-		Usuario usuario = new Usuario();
-		usuario.setEmail("usuario@gmail.com");
-		usuarioRepository.save(usuario);
+		when(usuarioRepository.existsByEmail("usuario@gmail.com")).thenReturn(Boolean.TRUE);
 		
 		assertThrows(RegraNegocioException.class,() -> usuarioService.validarEmail("usuario@gmail.com"));
 	}
