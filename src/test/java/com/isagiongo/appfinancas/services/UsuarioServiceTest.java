19a9805db892cb722ffcf5e25736e804dda80126
Isa Giongo <isagiongo@gmail.com>
2020-02-24 16:22:01 -0300

    Adiciona testes unitários para método de autenticar usuário

--- src/test/java/com/isagiongo/appfinancas/services/UsuarioServiceTest.java
+++ src/test/java/com/isagiongo/appfinancas/services/UsuarioServiceTest.java
@@ -1,8 +1,12 @@
 package com.isagiongo.appfinancas.services;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,7 +16,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.isagiongo.appfinancas.exceptions.AutenticacaoException;
 import com.isagiongo.appfinancas.exceptions.RegraNegocioException;
+import com.isagiongo.appfinancas.model.entities.Usuario;
 import com.isagiongo.appfinancas.repositories.UsuarioRepository;
 import com.isagiongo.appfinancas.services.impl.UsuarioServiceImpl;
 
@@ -43,6 +49,50 @@ public class UsuarioServiceTest {
 		when(usuarioRepository.existsByEmail("usuario@gmail.com")).thenReturn(Boolean.TRUE);
 		
 		assertThrows(RegraNegocioException.class,() -> usuarioService.validarEmail("usuario@gmail.com"));
+	}
+	
+	@Test
+	public void deveAutenticarUsuario() {
+		Usuario usuario = criaUsuario();
+		
+		when(usuarioRepository.findByEmail(usuario.getEmail())).thenReturn(Optional.of(usuario));
+		
+		Usuario resultado = usuarioService.autenticar(usuario.getEmail(), usuario.getSenha());
+		
+		assertNotNull(resultado);
+	}
+	
+	@Test
+	public void deveRetornarExcecaoSeUsuarioNaoForEncontrado() {
+		Usuario usuario = criaUsuario();
+		
+		when(usuarioRepository.findByEmail(usuario.getEmail())).thenReturn(Optional.empty());
+		
+		Exception exception = assertThrows(AutenticacaoException.class,
+				()-> usuarioService.autenticar(usuario.getEmail(), usuario.getSenha()));
+		
+		assertTrue(exception.getMessage().contains("Usuário não encontrado"));
+	}
+	
+	@Test
+	public void deveRetornarExcecaoSeSenhaNaoEstiverCorreta() {
+		Usuario usuario = criaUsuario();
+		
+		when(usuarioRepository.findByEmail(usuario.getEmail())).thenReturn(Optional.of(usuario));
+		
+		Exception exception = assertThrows(AutenticacaoException.class,
+				()-> usuarioService.autenticar(usuario.getEmail(), "55555"));
+		
+		assertTrue(exception.getMessage().contains("Senha inválida"));
+	}
+	
+	public static Usuario criaUsuario() {
+		Usuario usuario = new Usuario();
+		usuario.setId(1L);
+		usuario.setNome("Isadora");
+		usuario.setSenha("1234");
+		usuario.setEmail("isa@gmail.com");
+		return usuario;
 	}
 
 }
